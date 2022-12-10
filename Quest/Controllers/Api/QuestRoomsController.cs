c5a118020b09b6ef4bc5dfe15c15edf5f2fce2f0
Alla Ponurko <aponurko@gmail.com>
2022-12-11 00:43:48 +0300

    116

---
+++ Quest/Controllers/Api/QuestRoomsController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Quest.Data;
+using Quest.Models;
+
+namespace Quest.Controllers.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QuestRoomsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public QuestRoomsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/ApiQuestRooms
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<QuestRoom>>> GetQuests(int? SubjectId)
+        {
+            var query = _context.Quests.AsQueryable();
+            if (SubjectId != null)
+            {
+                query = query.Where(p => p.Subject.Id == SubjectId);
+            }
+            return await query.ToListAsync();
+            //return await _context.Quests.ToListAsync();
+        }
+
+        // GET: api/ApiQuestRooms/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<QuestRoom>> GetQuestRoom(int id)
+        {
+            var questRoom = await _context.Quests.FindAsync(id);
+
+            if (questRoom == null)
+            {
+                return NotFound();
+            }
+
+            return questRoom;
+        }
+
+        
+
+            
+        
+
+       
+        
+
+        private bool QuestRoomExists(int id)
+        {
+            return _context.Quests.Any(e => e.Id == id);
+        }
+    }
+}
--- Quest/Controllers/Api/SubjectsController.cs
+++ Quest/Controllers/Api/SubjectsController.cs
@@ -12,47 +12,47 @@ namespace Quest.Controllers.Api
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ApiQuestRoomsController : ControllerBase
+    public class SubjectsController : ControllerBase
     {
         private readonly ApplicationDbContext _context;
 
-        public ApiQuestRoomsController(ApplicationDbContext context)
+        public SubjectsController(ApplicationDbContext context)
         {
             _context = context;
         }
 
-        // GET: api/ApiQuestRooms
+        // GET: api/Subjects
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<QuestRoom>>> GetQuests()
+        public async Task<ActionResult<IEnumerable<Subject>>> GetSubjects()
         {
-            return await _context.Quests.ToListAsync();
+            return await _context.Subjects.ToListAsync();
         }
 
-        // GET: api/ApiQuestRooms/5
+        // GET: api/Subjects/5
         [HttpGet("{id}")]
-        public async Task<ActionResult<QuestRoom>> GetQuestRoom(int id)
+        public async Task<ActionResult<Subject>> GetSubject(int id)
         {
-            var questRoom = await _context.Quests.FindAsync(id);
+            var subject = await _context.Subjects.FindAsync(id);
 
-            if (questRoom == null)
+            if (subject == null)
             {
                 return NotFound();
             }
 
-            return questRoom;
+            return subject;
         }
 
-        // PUT: api/ApiQuestRooms/5
+        // PUT: api/Subjects/5
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPut("{id}")]
-        public async Task<IActionResult> PutQuestRoom(int id, QuestRoom questRoom)
+        public async Task<IActionResult> PutSubject(int id, Subject subject)
         {
-            if (id != questRoom.Id)
+            if (id != subject.Id)
             {
                 return BadRequest();
             }
 
-            _context.Entry(questRoom).State = EntityState.Modified;
+            _context.Entry(subject).State = EntityState.Modified;
 
             try
             {
@@ -60,7 +60,7 @@ namespace Quest.Controllers.Api
             }
             catch (DbUpdateConcurrencyException)
             {
-                if (!QuestRoomExists(id))
+                if (!SubjectExists(id))
                 {
                     return NotFound();
                 }
@@ -73,36 +73,36 @@ namespace Quest.Controllers.Api
             return NoContent();
         }
 
-        // POST: api/ApiQuestRooms
+        // POST: api/Subjects
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPost]
-        public async Task<ActionResult<QuestRoom>> PostQuestRoom(QuestRoom questRoom)
+        public async Task<ActionResult<Subject>> PostSubject(Subject subject)
         {
-            _context.Quests.Add(questRoom);
+            _context.Subjects.Add(subject);
             await _context.SaveChangesAsync();
 
-            return CreatedAtAction("GetQuestRoom", new { id = questRoom.Id }, questRoom);
+            return CreatedAtAction("GetSubject", new { id = subject.Id }, subject);
         }
 
-        // DELETE: api/ApiQuestRooms/5
+        // DELETE: api/Subjects/5
         [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteQuestRoom(int id)
+        public async Task<IActionResult> DeleteSubject(int id)
         {
-            var questRoom = await _context.Quests.FindAsync(id);
-            if (questRoom == null)
+            var subject = await _context.Subjects.FindAsync(id);
+            if (subject == null)
             {
                 return NotFound();
             }
 
-            _context.Quests.Remove(questRoom);
+            _context.Subjects.Remove(subject);
             await _context.SaveChangesAsync();
 
             return NoContent();
         }
 
-        private bool QuestRoomExists(int id)
+        private bool SubjectExists(int id)
         {
-            return _context.Quests.Any(e => e.Id == id);
+            return _context.Subjects.Any(e => e.Id == id);
         }
     }
 }
